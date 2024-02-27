9905503c1858894ad2a174257e404bb63c761d72
alandlan <alanhorcrux@gmail.com>
2024-02-27 17:08:56 -0300

    install styled components

--- dailydiet/src/views/home/index.tsx
+++ dailydiet/src/views/home/index.tsx
@@ -1,17 +1,10 @@
 import { View, Text, StyleSheet} from 'react-native';
+import { Container } from './styles';
 
 export default function Home() {
     return (
-        <View style={styles.container}>
+        <Container>
             <Text>Home</Text>
-        </View>
+        </Container>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-});
