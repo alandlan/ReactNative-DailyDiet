c9785e5defc92c33aaad38d49ecda005aa1b1c98
alandlan <alanhorcrux@gmail.com>
2024-02-27 17:36:07 -0300

    ajust theme

--- dailydiet/src/views/home/styles.ts
+++ dailydiet/src/views/home/styles.ts
@@ -1,9 +1,8 @@
 import styled from "styled-components/native";
 
-
 export const Container = styled.View`
     flex: 1;
     justify-content: center;
     align-items: center;
-    background-color: #ccc;
+    background-color: ${({ theme }) => theme.COLORS.GRAY_7};
 `;
